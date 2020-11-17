683ec794e261c2803c6d149078b53cf87c2062ab
adams66 <adamsdalton14@gmail.com>
2020-11-17 12:51:52 -0500

    git commit

--- script.js
+++ script.js
@@ -6,12 +6,18 @@ function toggle(){
     
 }
 
+function noScroll() {
+    window.scrollTo(0, 0);
+  }
+
 function openNav(){
     document.getElementById('hamburger-display').style.display="flex";
     document.getElementById('hamburger').style.display="none";
+    window.addEventListener('scroll', noScroll);
 }
 
 function closeNav(){
     document.getElementById('hamburger-display').style.display="none";
     document.getElementById('hamburger').style.display="block";
+    window.removeEventListener('scroll', noScroll);
 }
